796b446110f1d577422af5a2b8fff106c975b810
a022928 <a022928@umaia.pt>
2026-06-15 22:11:32 +0100

    Add files via upload
    Alteração da página index.html
    Modificação da apresentação powerpoint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector(".menu");
+const menu    = document.querySelector(".menu");
 const navMenu = document.querySelector(".nav-menu");
 
 menu.addEventListener("click", function () {
